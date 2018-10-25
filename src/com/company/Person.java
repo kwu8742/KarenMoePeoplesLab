9ee624200332d82efc218213388cfb4153967d71
BT_1N3_07 <kwu8742@bths.edu>
2018-10-25 08:42:55 -0400

    hi

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class Person {
+    private String FirstName;
+    private String FamilyName;
+
+    public Person(String FirstName, String FamilyName) {
+        this.FirstName = FirstName;
+        this.FamilyName = FamilyName;
+    }
+
+    public String getFirstName() {
+        return this.FirstName;
+    }
+
+    public String getFamilyName() {
+        return this.FamilyName;
+    }
+
+    public String toString() {
+        return "First Name: " + this.FirstName + "Family Name: " + this.FamilyName;
+    }
 }
